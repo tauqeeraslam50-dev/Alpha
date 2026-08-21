7f78fc4d07b1786571645ce9ea1629e96d6f2338
tauqeeraslam50-dev <tauqeeraslam50@gmail.com>
2026-08-21 19:51:04 +0500

    feat(gis): add real offline GIS data manager IPC

--- preload.cjs
+++ preload.cjs
@@ -3,5 +3,11 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('rnmsOffline', {
   getMapInfo: () => ipcRenderer.invoke('offline-map-info'),
   loadDemTile: (tileName) => ipcRenderer.invoke('offline-dem-tile', tileName),
-  listDemTiles: () => ipcRenderer.invoke('offline-dem-list')
+  listDemTiles: () => ipcRenderer.invoke('offline-dem-list'),
+  selectMapFiles: () => ipcRenderer.invoke('offline-select-map-files'),
+  selectDemFolder: () => ipcRenderer.invoke('offline-select-dem-folder'),
+  installMapFiles: (files) => ipcRenderer.invoke('offline-install-map-files', files),
+  installDemFolder: (folder) => ipcRenderer.invoke('offline-install-dem-folder', folder),
+  removeMapAsset: (name) => ipcRenderer.invoke('offline-remove-map-asset', name),
+  validateAssets: () => ipcRenderer.invoke('offline-validate-assets')
 });
