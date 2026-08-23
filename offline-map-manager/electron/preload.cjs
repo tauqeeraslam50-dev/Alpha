cbf1ee3735aa659d7ee85f40153ce9dae9e9da17
tauqeeraslam50-dev <tauqeeraslam50@gmail.com>
2026-08-23 09:54:32 +0500

    Expose map folder scanning to renderer

--- offline-map-manager/electron/preload.cjs
+++ offline-map-manager/electron/preload.cjs
@@ -2,4 +2,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('mapManager', {
   selectMapFolder: () => ipcRenderer.invoke('select-map-folder'),
+  scanMapFolder: (folder) => ipcRenderer.invoke('scan-map-folder', folder),
 });
